12b29891cc0e16e061ddc663a2873590f10fcc5f
Ty <tyschultz@icloud.com>
2018-12-08 17:24:12 -0500

    Initial Commit

---
+++ MorningBrew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MorningBrew
+//
+//  Created by Ty Schultz on 12/8/18.
+//  Copyright © 2018 Ty Schultz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
